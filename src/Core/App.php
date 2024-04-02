083f33ebe25bd663ef9325c5f7809cff13caf6dc
Victor Valentim <jvvalentim.096@gmail.com>
2024-04-02 14:34:59 -0300

    ♻️ refactor(core): add missing type hints

--- src/Core/App.php
+++ src/Core/App.php
@@ -5,19 +5,19 @@ namespace Core;
 class App {
     protected static $container;
 
-    public static function setContainer($container) {
+    public static function setContainer(Container $container): void {
         static::$container = $container;
     }
 
-    public static function container() {
+    public static function container(): Container {
         return static::$container;
     }
 
-    public static function bind($key, $resolver) {
+    public static function bind(string $key, mixed $resolver): void {
         static::$container->bind($key, $resolver);
     }
 
-    public static function resolve($key) {
+    public static function resolve(string $key): mixed {
         return static::$container->resolve($key);
     }
 }
--- src/Core/Container.php
+++ src/Core/Container.php
@@ -7,11 +7,11 @@ use Exception;
 class Container {
     protected $bindings = [];
 
-    public function bind($key, $resolver) {
+    public function bind(string $key, callable $resolver): void {
         $this->bindings[$key] = $resolver;
     }
 
-    public function resolve($key) {
+    public function resolve(string $key): mixed {
         if (!array_key_exists($key, $this->bindings)) {
             throw new Exception("No matching key '{$key}' on this container.");
         }

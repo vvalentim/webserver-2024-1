271008ccdc774830eeb68c5ce4a73d6e98b8311c
Victor Valentim <jvvalentim.096@gmail.com>
2024-04-12 12:06:50 -0300

    ✨ feat(core): adiciona classe de validação genérica

---
+++ src/Core/Validator.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Core;
+
+class Validator {
+    public static function isEmpty(string $text): bool {
+        return empty(trim($text));
+    }
+
+    public static function isOnLengthRange(string $text, int $min, int $max): bool {
+        return strlen($text) >= $min && strlen($text) <= $max;
+    }
+
+    public static function hasNumber(string $text): bool {
+        return preg_match("/\d/", $text);
+    }
+
+    public static function hasLetter(string $text): bool {
+        return preg_match("/[a-z]/i", $text);
+    }
+
+    public static function hasLowercase(string $text): bool {
+        return !ctype_upper($text);
+    }
+
+    public static function hasUppercase(string $text): bool {
+        return !ctype_lower($text);
+    }
+
+    public static function hasSpecialCharacter(string $text): bool {
+        return !ctype_alnum($text);
+    }
+}
